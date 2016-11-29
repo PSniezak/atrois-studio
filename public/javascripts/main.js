5dd08efda54f5083ec4d41c2494a6ceafe49108a
Administrateur <dsi@synerghetic.net>
2016-11-29 17:35:06 +0100

    Addtional : logo / Copyright / Socials / Gradiant & Homepage : Hide Social / Arrow down

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,18 +7,30 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		scrollBar: false,
 		autoScrolling: true,
-		fixedElements: '#header, #footer',
+		fixedElements: '#header, #additional',
 
 		onLeave: function(index, nextIndex, direction) {
 			if (index == 1 && direction == "down") {
+				$('#additional .social').fadeIn();
+
 				gradientTimeout = setTimeout(function() {
+					$('#additional .gradient-reverse').fadeIn();
 					$('#header .container').addClass('gradient');
 				}, 2000);
+
 			} else if (nextIndex == 1 && direction == "up") {
 				clearTimeout(gradientTimeout);
+
+				$('#additional .social').fadeOut();
+				$('#additional .gradient-reverse').fadeOut();
 				$('#header .container').removeClass('gradient');
 			}
 		}
+	});
+
+	// Home
+	$('#section-home .arrow-down a').on('click', function() {
+		$.fn.fullpage.moveSectionDown();
 	});
 
 });
